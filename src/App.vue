301c07391d0302775f4abb8f11ba6491108eb40c
Deep Kumbhare <kumbharedeep85@gmail.com>
2023-05-03 11:07:24 +0530

    renamed file

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <DataTable />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+import DataTable from '@/components/DataTable.vue'
 </script>
